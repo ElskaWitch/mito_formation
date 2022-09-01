57e037865727db0932d060c48e0a6f79adbe49c1
ElskaWitch <justinelabas95@gmail.com>
2022-09-01 11:39:56 +0200

    responsive bck

--- backOffice.php
+++ backOffice.php
@@ -1,10 +1,13 @@
 <?php
+$title = "BackOffice";
 include("partials/_header.php")
 ?>
 <main class="">
-    <div class="parent flex justify-between ">
+    <div class="parent lg:flex  ">
         <?php include("partials/_left-nav.php") ?>
-        <?php include("partials/_bck-right.php") ?>
+        <div class="p-10">
+            <?php include("partials/_bck-right.php") ?>
+        </div>
     </div>
     <?php
     include("partials/_footer.php")
--- partials/_left-nav.php
+++ partials/_left-nav.php
@@ -1,4 +1,4 @@
- <div class="nav_left bg-gray-600 text-white h-screen sticky top-0 w-[25%] px-20  grid place-content-center  ">
+ <div class="nav_left bg-gray-600 text-white h-screen lg:sticky top-0 lg:w-[25%] px-20  grid place-content-center  ">
      <div class="py-20 text-xl">
          <h2 class="pb-5 underline">Formateurs</h2>
          <ul class="">
